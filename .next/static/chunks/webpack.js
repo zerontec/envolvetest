d858e7e68cd3e0f2c511fe52ac5337d38f7d65c5
zerontec <zerontecnologia1@gmail.com>
2024-01-29 10:03:40 -0400

    cchenges statict

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6a6bb62d28e907a6"; }
+/******/ 		__webpack_require__.h = function() { return "aee69b2ddb747f67"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7377101def92f5c1"; }
+/******/ 	__webpack_require__.h = function() { return "e394dbd2007a52d4"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
