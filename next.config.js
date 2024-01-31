68de4774097f1c33c798539f532db8c893f3fa13
zerontec <zerontecnologia1@gmail.com>
2024-01-30 23:18:08 -0400

    confi next pajage jason styles

--- next.config.js
+++ next.config.js
@@ -2,3 +2,15 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     basePath:'/envolvetest',
+//     assetPrefix:'/https://github.com/zerontec/envolvetest',
+//     output: 'out'
+// }
+
+// module.exports = nextConfig
+    
